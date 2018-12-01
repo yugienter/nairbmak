e40bd9f7b839b85200f365af048bf4a10e59b9ec
yugienter <phu.nguyenluu@gmail.com>
2018-12-02 01:49:07 +0700

    add file eth. config

--- src/config/eth.config.js
+++ src/config/eth.config.js
@@ -11,6 +11,12 @@ config.local = {
     ADDRESS: '0xa5c502edfc072fd5913e7879cc78fadb931aa0f7',
     DECIMALS: 18
   },
+  DATABASE: {
+    ADDRESS: '0x7369539f3ee65917411c1164f51a63090b30febb'
+  },
+  DISTRIBUTION: {
+    ADDRESS: '0x65d96bf68e2678ddb9a09b1153665c43198eeaf2'
+  },
   NETWORK: '4',
   EVENTS: {
     TRANSFER: 'Transfer'
