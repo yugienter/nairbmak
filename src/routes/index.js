74960a0e7036d4497315d980ce41cb381e3e9ca3
Loc Tran <loc_tran@kambria.io>
2018-12-02 00:40:33 +0700

    add page Sharing

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,13 @@ import KambriaComponents from 'views/pages/KambriaComponents';
 import BootstrapComponents from 'views/pages/BootstrapComponents';
 import Redux from 'views/pages/Redux';
 import Report from 'views/pages/Report';
+import AdrSharing from 'views/pages/AdrSharing';
 
 export default [
   { path: "/", component: Home, type: 'public' },
   { path: "/kambria-components", component: KambriaComponents, type: 'private' },
   { path: "/bootstrap-components", component: BootstrapComponents, type: 'private' },
   { path: "/redux", component: Redux, type: 'private' },
-  { path: "/report", component: Report, type: 'public' }
+  { path: "/report", component: Report, type: 'public' },
+  { path: "/adr-sharing", component: AdrSharing, type: 'public' }
 ];
---
+++ src/views/pages/AdrSharing.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Input, Button } from 'antd';
+
+class AdrSharing extends React.Component {
+
+  render() {
+
+    return (
+      <div id="site_wrapper">
+        <div className="site-upper adr-sharing">
+          {this.props.header}
+          <div className="page-hero">
+            <h1 className="heading">asdfsad</h1>
+            <span className="description">asdfas</span>
+          </div>;
+        </div>
+        <section className='AdrSharing__main'>
+          <form onSubmit={this.redeem} className="form-horizontal" id="redeem-form">
+            <div className="form-group">
+              <label htmlFor="redeem-address">Eth address</label>
+              <input value={'asdfas'} onChange={this.onChangeAddress} type="text" className={"form-control "} id="redeem-address" name="redeem-address" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="redeem-code">Redeem code</label>
+              <input value={'asfs'} onChange={this.onChangeCode} type="text" className={"form-control "} id="redeem-code" name="redeem-code" />
+            </div>
+            <div className="form-group">
+              <button id="redeem-submit" form="redeem-form" type="submit" className="btn btn-default">Withdraw</button>
+            </div>
+          </form>
+        </section>
+      </div>
+    );
+  }
+}
+export default AdrSharing;
